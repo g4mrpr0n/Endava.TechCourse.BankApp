148cbb0d2ee92d8740713e850a7b39d1d8ece07d
g4mrpr0n <georgeana.globa@gmail.com>
2023-10-18 18:39:43 +0300

    created domain

---
+++ Endava.TechCourse.BankApp.Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Endava.TechCourse.BankApp.Domain.Common
+{
+    internal class BaseEntity
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public DateTime TimeStamp { get; } = DateTime.Now;
+    }
+}
---
+++ Endava.TechCourse.BankApp.Domain/Models/Currency.cs
@@ -0,0 +1,12 @@
+﻿using Endava.TechCourse.BankApp.Domain.Common;
+
+namespace Endava.TechCourse.BankApp.Domain.Models
+{
+    internal class Currency : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string CurrencyCode { get; set; }
+        public decimal ChangeRate { get; set; }
+    }
+}
