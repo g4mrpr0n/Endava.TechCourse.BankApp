c99daec045e19caee5dc8bb0ad87b55a20bf6bc0
g4mrpr0n <georgeana.globa@gmail.com>
2023-10-23 14:07:15 +0300

    T 2 homework

---
+++ Endava.TechCourse.BankApp.Domain/Models/CashByCode.cs
@@ -0,0 +1,12 @@
+﻿using Endava.TechCourse.BankApp.Domain.Common;
+
+namespace Endava.TechCourse.BankApp.Domain.Models
+{
+    internal class CashByCode : BaseEntity
+    {
+        public Wallet Wallet { get; set; }
+        public Currency Currency { get; set; }
+        public decimal Amount { get; set; }
+        public uint Code { get; set; }
+    }
+}
---
+++ Endava.TechCourse.BankApp.Domain/Models/Report.cs
@@ -0,0 +1,12 @@
+﻿using Endava.TechCourse.BankApp.Domain.Common;
+
+namespace Endava.TechCourse.BankApp.Domain.Models
+{
+    internal class Report : BaseEntity
+    {
+        public List<Transaction> Transactions { get; set; }
+        public decimal TotalAmount { get; set; }
+        public Wallet Wallet { get; set; }
+        public Currency Currency { get; set; }
+    }
+}
